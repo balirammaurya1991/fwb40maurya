5218ee62ad615787f42757d85479622f302a2f1d
balirammaurya1991 <89415721+balirammaurya1991@users.noreply.github.com>
2021-11-03 01:31:50 -0500

    updated code

--- routes/university.js
+++ routes/university.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+class University{
+  constructor(university_name,university_location,university_type)
+  {
+    this.university_name = university_name;
+    this.university_location= university_location;
+    this.university_type = university_type;
+  }
+}
+var uni1 = new University("Northwest","Marryville","Public");
+var uni2 = new University("UCM","Marryville","Public");
+var uni3 = new University("Cleveland state university","Cleveland","Public");
+
 router.get('/', function(req, res, next) {
-  res.render('university', { title: 'Search Results university' });
+  res.render('university', { title: 'Search Results university',university:[uni1,uni2,uni3] });
 });
 
 module.exports = router;
